f1f9e7cf1e938c26441fc0ceb5e84dc79af65d23
DoctorWhose <toniserfling@aol.com>
2017-05-22 13:35:18 +0200

    Version 2
    - TriHard STOLEN Relu from Joshua
    - Selection now based on average of x number of wins
    - Learning variables now definable

--- GEN2_NN.py
+++ GEN2_NN.py
@@ -1,21 +1,26 @@
 import random
+import time
 from GAMENEW import Game,Dir
 import numpy as np
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure.modules.linearlayer import LinearLayer
 from pybrain.structure.modules.sigmoidlayer import SigmoidLayer
 from pybrain.structure.connections.full import FullConnection
+from numpy import average
 
-
+def rectify(value):
+    return (0, 1)[value <= 0.5]
 
 def play():
     
-    val= net.activate(game.gamefield.getAs1DArray())
-    if val <= 0.1:     
+    field_arr = game.gamefield.getAs1DArray()
+    out1, out2 = map(rectify, net.activate(field_arr))
+
+    if out1 and out2:
         return Dir.NORTH
-    elif val <=0.5:
+    elif not out1 and out2:
         return Dir.EAST
-    elif val <=0.9:
+    elif out1 and not out2:
         return Dir.WEST
     else:
         return Dir.SOUTH
@@ -31,15 +36,20 @@ def buildPopulation():
     return population
 
 def selection(population):
-    scores = [] 
+    
+    averageScores=[]
     selectedIndividuals = []
     for i in range(len(population)):
+        scores = [] 
         net._setParameters(population[i])
-        sc = game.runGame(play)
-        scores.append(sc)
+        for j in range(gamesPerIndividual):
+            sc = game.runGame(play)
+            scores.append(sc)
+        averageScores.append(sum(scores)/len(scores))
+    print sorted(averageScores, reverse = True)
     for i in range(20):
-        selectedIndividuals.append(population[scores.index(max(scores))])
-        scores.pop(scores.index(max(scores)))
+        selectedIndividuals.append(population[averageScores.index(max(averageScores))])
+        averageScores.pop(averageScores.index(max(averageScores)))
     return selectedIndividuals
     
 def crossover(selectedIndividuals): 
@@ -63,29 +73,35 @@ def mutate(population):
     return population
 
 def doGenetic(population):
-    mutationChance = 0.1
+
     newPopulation = population    
-    for i in range(1000):
+    for i in range(generationCount):
+        time0= time.time()
         selectedIndividuals = selection(newPopulation)
-        print "Selection No.", i
         newPopulation = crossover(selectedIndividuals)
-        print "Crossover No.",i
         if np.random.random() < mutationChance:
-            print "mutation occurred"
             newPopulation = mutate(newPopulation)
-    
-    for individual in newPopulation:
-        scores=[]
+        time1 = time.time()
+        print time1-time0
+    finalSelection = selection(newPopulation)
+    print "Doing final selection"
+    finalAverages = []
+    for individual in finalSelection:
         net._setParameters(individual)
+        scores=[]
         for i in range(100):
             sc=game.runGame(play)
             scores.append(sc)
-        print "Average: ", sum(scores)/len(scores)
+        finalAverages.append(sum(scores)/len(scores))
+    
+    print finalSelection
+    fittestIndividual = finalSelection[finalAverages.index(max(finalAverages))]
+    return fittestIndividual
 
 net = FeedForwardNetwork()
 inLayer = LinearLayer(16)
 hiddenLayer = SigmoidLayer(10)
-outLayer = SigmoidLayer(1)
+outLayer = SigmoidLayer(2)
 net.addInputModule(inLayer)
 net.addModule(hiddenLayer)
 net.addOutputModule(outLayer)
@@ -95,8 +111,14 @@ net.addConnection(in_to_hidden)
 net.addConnection(hidden_to_out)
 net.sortModules()
 
+'''Lernvariablen'''
+
+gamesPerIndividual = 30 #Anzahl Spiele per Individuum um Fitness zu bestimmen (10-50 max.)
+generationCount = 500 #Anzahl an Generationen
+mutationChance = 0.2 #Chance des Mutierens eines Individuums
+
 game = Game(None,4,4)
 population = buildPopulation()
-doGenetic(population)
+ultimateParams = doGenetic(population)
 
 
